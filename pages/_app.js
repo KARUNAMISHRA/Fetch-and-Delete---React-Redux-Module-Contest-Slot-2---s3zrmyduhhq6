c304d1e07f2326d6b65106ca04fef47a1326d313
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-12-17 18:43:22 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,31 @@
 import "@/styles/globals.css";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 export const AuthContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
   const [data, setData] = useState([]);
-  const [msg, setMsg] = useState("")
+  const [msg, setMsg] = useState("");
 
-  const API = "https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses"
+  useEffect(() => {
+    // Fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
+    fetchData();
+  }, []); // Run this effect only once on component mount
 
   const contextValue = {
-
+    data,
+    setData,
+    msg,
+    setMsg,
   };
 
   return (
