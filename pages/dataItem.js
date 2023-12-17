70ccc442d49d537003e9498145dffb9f6d8ac855
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-12-17 18:55:20 +0530

    Update dataItem.js

--- pages/dataItem.js
+++ pages/dataItem.js
@@ -1,18 +1,23 @@
 import { useContext } from "react";
 import { AuthContext } from "./_app";
 
-function DataItem({ }) {
-    const { } = useContext(AuthContext);
+function DataItem({ id, busName, ticketPrice, source, destination, onDelete }) {
+  const { msg } = useContext(AuthContext);
 
-    return (
-        <div className="data">
-            <div></div>
-            <div>Bus Name: </div>
-            <div>/-</div>
-            <div>From: </div>
-            <div>To: </div>
-            <button>Delete</button>
-        </div>
-    );
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="data">
+      <div>ID: {id}</div>
+      <div>Bus Name: {busName}</div>
+      <div>Ticket Price: {ticketPrice}</div>
+      <div>From: {source}</div>
+      <div>To: {destination}</div>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 }
+
 export default DataItem;
